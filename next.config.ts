8f2860e0f1d59e124413a4003b6854a7f9947ed6
Luke <40731149+lukeae2001@users.noreply.github.com>
2025-04-21 19:21:27 +1000

    fix: Add output export to next config and changed to main

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // <<< THIS LINE IS NEEDED for static export
+  /* other config options can go here later if needed */
 };
 
 export default nextConfig;
